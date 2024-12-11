abe7787b683697a86a1dda9dec01410956893061
Your Name <you@example.com>
2024-12-11 02:12:37 +0200

    create theme and create notes view

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/notes_view.dart';
+
+void main() {
+  runApp(NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      home: NotesView(),
+    );
+  }
+}
